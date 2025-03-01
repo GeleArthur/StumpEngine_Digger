4f968c5eee7ddebfd00ec951026b5964eb5ec82d
GeleArthur <geleminecrafter@gmail.com>
2025-03-01 15:08:38 +0100

    Added template function and fixed the cache not working

--- src/ImguiTashTheCache.cpp
+++ src/ImguiTashTheCache.cpp
@@ -18,51 +18,45 @@ void ImguiTashTheCache::update()
 
 void ImguiTashTheCache::render()
 {
+    exercise1();
+    exercise2();
+}
+
+void ImguiTashTheCache::exercise1()
+{
     ImGui::SetNextWindowSize({400, 450}, ImGuiCond_Appearing);
     ImGui::Begin("Exercise1");
 
     ImGui::InputInt("Samples", &m_exercise1_samples);
-
-    std::array<int, exercise_skip_levels> m_exercise1_x_data;
-
-    constexpr auto m_exercise1_x_data2 =
-        std::views::iota(0, exercise_skip_levels)
-        | std::views::transform([](const int n)
-        {
-            return static_cast<int>(std::pow(2, n));
-        });
-    std::ranges::copy(m_exercise1_x_data2, m_exercise1_x_data.begin());
-
-
     if (ImGui::Button("Trash The Cache"))
     {
+        trash_the_cache<int, false>(m_exercise1_y_data, m_exercise1_samples);
+        /*std::vector<int> largeArray(2'000'000'0);
         m_exercise1_y_data.clear();
-        std::vector<int> array_to_mess_with(1000'000'0, 3);
-        for (const int skip_index : m_exercise1_x_data)
+        for (auto stepsize = 1; stepsize <= 1024; stepsize *= 2)
         {
-            std::vector<int> time_record(m_exercise1_samples);
-            for (int sample_count = 0; sample_count < m_exercise1_samples; ++sample_count)
+            std::vector<int> array_of_times;
+            for (int u = 0; u < m_exercise1_samples; ++u)
             {
                 using namespace std::chrono;
-                auto start_time = high_resolution_clock::now();
-                for (size_t i = 0; i < array_to_mess_with.size(); i += skip_index)
-                {
-                    array_to_mess_with[i] *= 2;
-                }
-                auto end_time = high_resolution_clock::now();
+                const auto start = high_resolution_clock::now();
 
-                const auto time_it_took = duration_cast<microseconds>(end_time - start_time).count();
-                time_record[sample_count] = static_cast<int>(time_it_took);
+                for (int i = 0; i < largeArray.size(); i += stepsize)
+                {
+                    largeArray[i] *= 2;
+                }
+                const auto end = high_resolution_clock::now();
+                const auto time_took = duration_cast<nanoseconds>(end - start).count();
+
+                array_of_times.push_back(static_cast<int>(time_took));
             }
 
-            std::ranges::sort(time_record);
-            auto results = std::ranges::subrange(++time_record.begin(), --time_record.end());
-            auto avarage =
-                std::accumulate(results.begin(), results.end(), 0)
-                / (m_exercise1_samples - 2);
+            std::ranges::sort(array_of_times);
+            int total = std::accumulate(array_of_times.begin() + 1, array_of_times.end() - 1, 0);
+            total = total / (m_exercise1_samples - 2);
 
-            m_exercise1_y_data.push_back(avarage);
-        }
+            m_exercise1_y_data.push_back(total);
+        }*/
     }
 
 
@@ -71,20 +65,152 @@ void ImguiTashTheCache::render()
         if (ImPlot::BeginPlot("Exercise1 plot"))
         {
             ImPlot::SetupAxesLimits(
-                *std::ranges::min_element(m_exercise1_x_data),
-                *std::ranges::max_element(m_exercise1_x_data),
+                0,
+                10,
                 *std::ranges::min_element(m_exercise1_y_data),
                 *std::ranges::max_element(m_exercise1_y_data)
             );
             ImPlot::PlotLine("Exercise1 line",
-                             m_exercise1_x_data.data(),
                              m_exercise1_y_data.data(),
                              static_cast<int>(m_exercise1_y_data.size()));
             ImPlot::EndPlot();
         }
     }
 
-    ImPlot::ShowDemoWindow();
+    ImGui::End();
+}
+
+
+struct Transform3D
+{
+    float matrix[16] = {
+        1, 0, 0, 0,
+        0, 1, 0, 0,
+        0, 0, 1, 0,
+        0, 0, 0, 1
+    };
+};
+
+class Gameobject3D
+{
+public:
+    Transform3D local = Transform3D{};
+    int id{};
+};
+
+class Gameobject3DAlt
+{
+public:
+    ~Gameobject3DAlt()
+    {
+        delete local;
+    }
+
+    Transform3D* local = new Transform3D{};
+    int id{};
+};
+
+
+void ImguiTashTheCache::exercise2()
+{
+    ImGui::SetNextWindowSize({400, 450}, ImGuiCond_Appearing);
+    ImGui::Begin("Exercise2");
+
+    ImGui::InputInt("Samples", &m_exercise2_samples);
+    if (ImGui::Button("Trash The Cache With GameObjects3D"))
+    {
+        trash_the_cache<Gameobject3D, true>(m_exercise2_y_data, m_exercise2_samples, 10'000);
+
+        /*std::vector<Gameobject3D> largeArray(2'000'000);
+        m_exercise2_y_data.clear();
+        for (auto stepsize = 1; stepsize <= 1024; stepsize *= 2)
+        {
+            std::vector<int> array_of_times;
+            for (int u = 0; u < m_exercise2_samples; ++u)
+            {
+                using namespace std::chrono;
+                const auto start = high_resolution_clock::now();
+
+                for (int i = 0; i < largeArray.size(); i += stepsize)
+                {
+                    largeArray[i].id *= 2;
+                }
+                const auto end = high_resolution_clock::now();
+                const auto time_took = duration_cast<nanoseconds>(end - start).count();
+
+                array_of_times.push_back(static_cast<int>(time_took));
+            }
+
+            std::ranges::sort(array_of_times);
+            int total = std::accumulate(array_of_times.begin() + 1, array_of_times.end() - 1, 0);
+            total = total / (m_exercise1_samples - 2);
+
+            m_exercise2_y_data.push_back(total);
+        }*/
+    }
+
+
+    if (!m_exercise2_y_data.empty())
+    {
+        if (ImPlot::BeginPlot("Exercise2 plot"))
+        {
+            ImPlot::SetupAxesLimits(
+                0,
+                10,
+                *std::ranges::min_element(m_exercise2_y_data),
+                *std::ranges::max_element(m_exercise2_y_data)
+            );
+            ImPlot::PlotLine("GameObject",
+                             m_exercise2_y_data.data(),
+                             static_cast<int>(m_exercise2_y_data.size()));
+            ImPlot::EndPlot();
+        }
+    }
+
+    if (ImGui::Button("Trash The Cache With GameObjects3DALT"))
+    {
+        trash_the_cache<Gameobject3DAlt, true>(m_exercise3_y_data, m_exercise2_samples, 100'000);
+    }
+
+
+    if (!m_exercise3_y_data.empty())
+    {
+        if (ImPlot::BeginPlot("Exercise3 plot"))
+        {
+            ImPlot::SetupAxesLimits(
+                0,
+                10,
+                *std::ranges::min_element(m_exercise3_y_data),
+                *std::ranges::max_element(m_exercise3_y_data)
+            );
+            ImPlot::PlotLine("GameObjectAlt",
+                             m_exercise3_y_data.data(),
+                             static_cast<int>(m_exercise3_y_data.size()));
+            ImPlot::EndPlot();
+        }
+    }
+
+    if (!m_exercise3_y_data.empty() && !m_exercise2_y_data.empty())
+    {
+        if (ImPlot::BeginPlot("Combined"))
+        {
+            ImPlot::SetupAxesLimits(
+                0,
+                10,
+                *std::ranges::min_element(m_exercise2_y_data),
+                *std::ranges::max_element(m_exercise2_y_data)
+            );
+            ImPlot::PlotLine("GameObject",
+                             m_exercise2_y_data.data(),
+                             static_cast<int>(m_exercise2_y_data.size()));
+
+            ImPlot::PlotLine("GameObjectAlt",
+                             m_exercise3_y_data.data(),
+                             static_cast<int>(m_exercise3_y_data.size()));
+            ImPlot::EndPlot();
+        }
+    }
+
 
     ImGui::End();
 }
--- src/ImguiTashTheCache.h
+++ src/ImguiTashTheCache.h
@@ -6,6 +6,7 @@
 #include <numeric>
 #include <algorithm>
 #include <complex>
+#include <chrono>
 
 #include "Component.h"
 
@@ -36,24 +37,59 @@ public:
 
     void update() override;
     void render() override;
+    void exercise1();
+    void exercise2();
 
 private:
     int m_exercise1_samples{10};
-    static constexpr int exercise_skip_levels{11};
-    // Why do you do this to me MSVC
-    // static constexpr std::array<int, exercise_skip_levels> m_exercise1_x_data = []()
-    // {
-    //     std::array<int, exercise_skip_levels> arr{};
-    //     std::ranges::copy(
-    //         std::views::iota(0, exercise_skip_levels) // Create array from 0,1,2,3...
-    //         | std::views::transform([](const int n) { return static_cast<int>(std::pow(2, n)); }),
-    //         // 2 of power n each number in the array
-    //         arr.begin());
-    //
-    //     return arr;
-    // }();
+    int m_exercise2_samples{100};
 
     std::vector<int> m_exercise1_y_data;
+    std::vector<int> m_exercise2_y_data;
+    std::vector<int> m_exercise3_y_data;
+
+    int m_exercise1_max_height{};
+    int m_exercise2_max_height{};
+    int m_exercise3_max_height{};
+    int m_exercise_combined_max_height{};
+
+    template <typename TypeToTest, bool hasID = false>
+    static void trash_the_cache(std::vector<int>& output, int sample_count = 10, int objects_count = 2'000'000'0)
+    {
+        std::vector<TypeToTest> largeArray(objects_count);
+        output.clear();
+        for (auto stepsize = 1; stepsize <= 1024; stepsize *= 2)
+        {
+            std::vector<int> array_of_times;
+            for (int u = 0; u < sample_count; ++u)
+            {
+                using namespace std::chrono;
+                const auto start = high_resolution_clock::now();
+
+                for (int i = 0; i < largeArray.size(); i += stepsize)
+                {
+                    if constexpr (hasID) // ????
+                    {
+                        largeArray[i].id *= 2;
+                    }
+                    else
+                    {
+                        largeArray[i] *= 2;
+                    }
+                }
+                const auto end = high_resolution_clock::now();
+                const auto time_took = duration_cast<nanoseconds>(end - start).count();
+
+                array_of_times.push_back(static_cast<int>(time_took));
+            }
+
+            std::ranges::sort(array_of_times);
+            int total = std::accumulate(array_of_times.begin() + 1, array_of_times.end() - 1, 0);
+            total = total / (sample_count - 2);
+
+            output.push_back(total);
+        }
+    }
 };
 
 
