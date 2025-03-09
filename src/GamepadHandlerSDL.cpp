83dd3a328f45094752b5e9676517241e92eb3c44
GeleArthur <geleminecrafter@gmail.com>
2025-03-09 22:15:11 +0100

    Yipeee linux

--- src/GamepadHandlerSDL.cpp
+++ src/GamepadHandlerSDL.cpp
@@ -11,9 +11,14 @@ void GamepadHandler::poll_gamepad() const
 {
 }
 
-void GamepadHandler::bind_gamepad_button(SDL_GamepadButton gamepad_button, input_pressed_type input_type, std::unique_ptr<Command>&& command) const
+void GamepadHandler::bind_gamepad_button(SDL_GamepadButton, input_pressed_type, std::unique_ptr<Command>&&) const
 {
 }
+
+void GamepadHandler::unbind_gamepad_button(SDL_GamepadButton, input_pressed_type) const
+{
+}
+
 
 GamepadHandler::GamepadHandler():
     m_handler_imlp{std::make_unique<GamepadHandlerImpl>()}
